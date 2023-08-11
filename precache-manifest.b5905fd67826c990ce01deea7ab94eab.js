cc3d5b48f0f618b4d2b00db672cd125224e152bc
Machmud Wahyu Domili <mudher.d@gmail.com>
2023-08-11 14:59:10 +0800

    Add files via upload

---
+++ precache-manifest.b5905fd67826c990ce01deea7ab94eab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9030f0021842bd2e81bf",
+    "url": "/static/css/main.4b834455.chunk.css"
+  },
+  {
+    "revision": "9030f0021842bd2e81bf",
+    "url": "/static/js/main.f3504a29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "a56c1133accd2c528b8dc26b70da5bc0",
+    "url": "/index.html"
+  }
+];
